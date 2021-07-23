fccee25955e0d5857e20125da8ea00dfb48a542a
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-23 20:44:36 -0300

    Create SE_BT+output.js

---
+++ src/ayudantehax/SE_BT+output.js
@@ -0,0 +1,59 @@
+/*
+Number.prototype.max = function(other) {
+  let _this = this.valueOf();
+  if (typeof other !== `number`) throw new Error(`unexpected parameter`);
+  else return (_this >= other && _this) || other;
+}
+*/
+
+Number.prototype.gray_code = function() {
+  let _this = this.valueOf();
+  if (!Number.isInteger(_this)) throw new Error(`unexpected number`);
+  else return _this ^ (_this >> 1);
+}
+
+class Tournament {
+  constructor(ranking) {
+    this.ranking = ranking;
+  }
+  
+  static create(teams) {
+    let head_node = new Tournament(1);
+    for (let ranking = 2; ranking <= teams; ranking++) {
+      head_node.add_team(ranking);
+    }
+    return head_node;
+  }
+  
+  add_team(ranking) {
+    return this.add_team_help(ranking, (ranking - 1).gray_code());
+  }
+  
+  add_team_help(ranking, gray_code) {
+    if (this.left === undefined) {
+      this.left   = new Tournament(this.ranking);
+      this.right  = new Tournament(ranking);
+    }
+    else if (gray_code % 2 == 0) this.left.add_team_help(ranking, gray_code >> 1);
+    else                          this.right.add_team_help(ranking, gray_code >> 1);
+  }
+  
+  /*
+  rounds() {
+    if (!this.left && this.left !== 0)  return 0;
+    else                                return 1 + (this.left.rounds().max(this.right.rounds()));
+  }
+  
+  round(level){
+    return this.round_help(this.rounds() - level);
+  }
+  
+  round_help(reverse_level){
+    if      (!this.left && this.left !== 0) return [[this.ranking, `bye`]];
+    else if (reverse_level === 0)           return [[this.left.ranking, this.right.ranking]];
+    else                                    return  this.left.round_help(reverse_level - 1) +
+                                                    this.right.round_help(reverse_level - 1)
+  }
+  */
+}
+
