8493ddbfa3ff8bd53c6110d7d2bcb8bae4156373
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-23 17:43:57 -0300

    Update SE.js

--- src/ayudantehax/SE.js
+++ src/ayudantehax/SE.js
@@ -13,6 +13,8 @@ Number.prototype.gray_code = function() {
 
 class Tournament {
   constructor(ranking) {
+    this.left = null;
+    this.right = null;
     this.ranking = ranking;
     this.create = function(teams){
       let head_node = new Tournament(1);
@@ -24,19 +26,30 @@ class Tournament {
   }
   
   add_team(ranking) {
-    return add_team_help(ranking, (ranking - 1).gray_code);
+    return this.add_team_help(ranking, (ranking - 1).gray_code);
   }
   
   rounds() {
+    if (this.left === null) return 0;
+    else                    return 1 + (this.left.rounds().max(this.right.rounds()));
   }
   
   round(level){
-    return round_help(rounds() - level);
+    return this.round_help(this.rounds() - level);
   }
   
   add_team_help(ranking, gray_code) {
+    if (this.left === null) {
+      this.left   = new Tournament(this.ranking);
+      this.right  = new Tournament(ranking);
+    }
+    else if (gray_code % 2 === 0) this.left.add_team_help(ranking, gray_code >> 1);
+    else                          this.right.add_team_help(ranking, gray_code >> 1);
   }
   
   round_help(reverse_level){
+    if      (this.left === null)  return [[this.ranking, `bye`]];
+    else if (reverse_level === 0) return [[this.left.ranking, this.right.ranking]];
+    else                          return this.left.round_help(reverse_level - 1) + this.right.round_help(reverse_level - 1)
   }
 }
