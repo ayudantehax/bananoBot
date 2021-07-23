2e98aaf81d550b41d47a5736cc9e37d8247a399f
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-23 17:10:02 -0300

    Update SE.js

--- src/ayudantehax/SE.js
+++ src/ayudantehax/SE.js
@@ -1,12 +1,42 @@
 
 Number.prototype.max = function(other) {
   let _this = this.valueOf();
-  if(typeof other !== `number`) throw new Error(`unexpected parameter`);
+  if (typeof other !== `number`) throw new Error(`unexpected parameter`);
   else return (_this >= other && _this) || other;
 }
 
-Number.prototype.toGrayCode = function() {
+Number.prototype.gray_code = function() {
   let _this = this.valueOf();
-  if(!Number.isInteger(_this)) throw new Error(`unexpected number`);
+  if (!Number.isInteger(_this)) throw new Error(`unexpected number`);
   else return _this ^ (_this >> 1);
+}
+
+class Tournament {
+  constructor(ranking) {
+    this.ranking = ranking;
+    this.create = function(teams){
+      let head_node = new Tournament(1);
+      for (let ranking = 2; ranking <= teams; ranking++) {
+        head_node.add_team(ranking);
+      }
+      return head_node;
+    }
+  }
+  
+  add_team(ranking) {
+    return add_team_help(ranking, (ranking - 1).gray_code);
+  }
+  
+  rounds() {
+  }
+  
+  round(level){
+    return round_help(rounds() - level);
+  }
+  
+  add_team_help(ranking, gray_code) {
+  }
+  
+  round_help(reverse_level){
+  }
 }
