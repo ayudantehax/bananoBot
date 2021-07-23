4c902b953769ef711f81d977987574f5fe533272
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-23 16:51:11 -0300

    Update SE.js

--- src/ayudantehax/SE.js
+++ src/ayudantehax/SE.js
@@ -1,1 +1,11 @@
 
+Number.prototype.max = function(other) {
+  if(typeof other !== `number`) throw new Error(`unexpected parameter`);
+  else return (this >= other && this.valueOf()) || other;
+}
+
+Number.prototype.toGrayCode = function() {
+  let _this = this.valueOf();
+  if(!Number.isInteger(_this)) throw new Error();
+  else return _this ^ (_this >> 1);
+}
