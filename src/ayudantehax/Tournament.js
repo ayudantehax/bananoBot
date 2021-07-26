57bc62c44c6effb9943124e3f4cb4a2280987e01
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-25 23:40:31 -0300

    Update Tournament.js

--- src/ayudantehax/Tournament.js
+++ src/ayudantehax/Tournament.js
@@ -8,13 +8,12 @@ class Match {
 class Tournament {
   
   constructor(teams) { // es N
-    this.date   = new Date();
-    this.games  = teams - 1; // si es que no se juega por el tercer puesto
-    this.rounds = Tournament.calculateRounds(teams); // añade uno si hay al menos una exención
-    this.byes   = Math.pow(2,this.rounds) - teams; // https://stackoverflow.com/a/22859838 (es P)
-    this.size   = {};
-    // si hay más de 2 exenciones. entonces 
-    this.size.rows = (this.byes > 2) ? (Math.pow(2, this.rounds) - 1) : (Math.pow(2, this.rounds - 1) - 1);
+    this.date         = new Date();
+    this.games        = teams - 1; // si es que no se juega por el tercer puesto
+    this.rounds       = Tournament.calculateRounds(teams); // añade uno si hay al menos una exención
+    this.byes         = Math.pow(2,this.rounds) - teams; // https://stackoverflow.com/a/22859838 (es P)
+    this.size         = {};
+    this.size.rows    = (this.byes > 2) ? (Math.pow(2, this.rounds) - 1) : (Math.pow(2, this.rounds - 1) - 1);
     this.size.columns = (this.rounds + 1 /* columna de ganador */) * 2 - 1;
   }
   
