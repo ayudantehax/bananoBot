d58f9b451368d21794b445c1cbfd576a57c2f93b
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-25 22:55:19 -0300

    Update Tournament.js

--- src/ayudantehax/Tournament.js
+++ src/ayudantehax/Tournament.js
@@ -1,12 +1,4 @@
 
-class Test {
-  static COUNTER = 0;
-  
-  static sumCounter() {
-    COUNTER++;
-  }
-}
-
 class Match {
   
   constructor() {
@@ -15,18 +7,18 @@ class Match {
 
 class Tournament {
   
-  constructor() {
-    this.date = new Date();
-    
+  constructor(teams) {
+    this.date   = new Date();
+    this.games  = teams - 1;
+    this.rounds = this.calculateRounds(teams); // añade uno si hay al menos una exención
+    this.byes   = Math.pow(2,this.rounds) - teams; // https://stackoverflow.com/a/22859838
+    this.size   = {};
+    this.size.rows = this.byes !== 0 ? : ; // total de juegos en la primera ronda (sin exenciones) o en la segunda ronda (con exenciones)
+    this.size.columns = this.rounds + 1; // + 1 (columna de ganador)
   }
   
-  static create(teams, type) {
-    new Tournament(teams, type);
-  }
-  
-  static generateSE() {
-  }
-  
-  static generateDE() {
+  static calculateRounds(teams, _n = 1){
+    if (teams <= Math.pow(2, _n)) return _n;
+    return calculateRounds(teams, _n + 1);
   }
 }
